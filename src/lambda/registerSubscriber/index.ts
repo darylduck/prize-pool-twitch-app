cee2fb9b6824a6f871ccfafeb4871427521fbb95
Daryl Duckmanton <daryl.duckmanton@gmail.com>
2021-06-23 20:49:11 +1000

    Update index.ts

--- src/lambda/registerSubscriber/index.ts
+++ src/lambda/registerSubscriber/index.ts
@@ -5,13 +5,13 @@ import * as graphql from 'graphql';
 import { isValidTwitchRequest } from './verify-twitch';
 
 export const handler = async (event: any, context: any) => {
-    const body = event.body3;
+    const body = event.body;
     const headers = event.headers;
     const twitchMessageType = headers['twitch-eventsub-message-type'];
     const verificationStatus = 'webhook_callback_verification';
     
     if (!body.challenge && twitchMessageType === verificationStatus) {
-        context.fail(JSON.stringify({
+        context.fail(SON.stringify({
             errorMessage:'Could not locate challenge value in your payload'
         }));
     }
